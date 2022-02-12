1162c34959a39cb65789f9c73f077b88a5cdae7e
unknown <damMota>
2022-02-12 17:23:44 -0300

    third commit

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+
+
+const error = {
+    color: 'blue',
+    backgroundColor: 'red',
+  };
+export default function Erro404() {
+    return(
+        <div>
+            <span  style={error}>Sem conteúdo</span>
+        </div>
+    )
+}
